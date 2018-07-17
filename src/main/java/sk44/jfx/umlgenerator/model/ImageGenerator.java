f72de874ceb0a380b13f9fd1b4588c18a3dcb098
sk44 <zombie.squad.69.cbt@gmail.com>
2018-07-17 12:57:32 +0900

    ファイルの文字コード自動判定処理を追加

--- src/main/java/sk44/jfx/umlgenerator/model/ImageGenerator.java
+++ src/main/java/sk44/jfx/umlgenerator/model/ImageGenerator.java
@@ -13,6 +13,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import net.sourceforge.plantuml.GeneratedImage;
 import net.sourceforge.plantuml.SourceFileReader;
+import org.mozilla.universalchardet.UniversalDetector;
 
 /**
  *
@@ -25,7 +26,8 @@ public class ImageGenerator {
             throw new IllegalArgumentException(textFile.toString() + " does not exists.");
         }
         try {
-            SourceFileReader reader = new SourceFileReader(textFile.toFile(), textFile.getParent().toFile(), "UTF-8");
+            String encoding = getEncodingFrom(textFile);
+            SourceFileReader reader = new SourceFileReader(textFile.toFile(), textFile.getParent().toFile(), encoding);
             List<GeneratedImage> list = reader.getGeneratedImages();
             File png = list.get(0).getPngFile();
             return Paths.get(png.getAbsolutePath());
@@ -33,4 +35,16 @@ public class ImageGenerator {
             throw new RuntimeException(ex);
         }
     }
+
+    private String getEncodingFrom(Path textFile) throws IOException {
+
+        UniversalDetector detector = new UniversalDetector(null);
+        byte[] data = Files.readAllBytes(textFile);
+        detector.handleData(data, 0, data.length);
+        detector.dataEnd();
+        String encoding = detector.getDetectedCharset();
+        detector.reset();
+        return encoding;
+    }
+
 }
